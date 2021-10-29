ee074cc7fbd976ab411eb1483381a1fd42a7635d
AlvaroFernandezRodriguez <44578868+AlvaroFernandezRodriguez@users.noreply.github.com>
2021-10-29 17:53:46 +0200

    final
    final

--- proyecto.php
+++ proyecto.php
@@ -2,23 +2,41 @@
 
 $mes= $_POST["Mes"];
 $dia= $_POST["Dia"];
-$fecha = array($mes,$dia);
 
-function buscar (){
-  $match
-}
 
-/*
+#echo $mes,$dia;
+
+ /* $fichero= file_get_contents("D:\\xampp\\apache\\logs\\error.log");
+  $partir = explode(" ",$fichero);
+  $encuentra=  $partir[1];
+  $pos= strpos($fichero,$encuentra);
+
+      if($pos !== false){
+        "No encontrado '$encuentra'";
+      }
+      else{
+        "Encontrado '$encuentra' en '$pos'";
+      }*/
+
+
+
+
+
 echo '<link rel="stylesheet" href="estilos.css" type="text/css">';
-  $fichero= fopen("D:\\xampp\\apache\\logs\\error.log", "r");
+$fichero = fopen("D:\\xampp\\apache\\logs\\error.log", "r");
  echo "<textarea>";
  while (!feof($fichero)){
-     $linea= fgets($fichero);
-     echo $linea;
- } 
+     $linea = fgets($fichero);
+     $buscar = strpos($linea,$mes." ".$dia);
+  if($buscar !== false){
+
+    echo $linea."<br>";
+
+  }
+ }
  fclose($fichero);
  echo "</textarea>"; 
- */
+
 
 /*$logs = "mes dia";
 $partir = explode(" ",$logs);
